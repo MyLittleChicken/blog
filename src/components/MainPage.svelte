744c04e992f50d4cb99002c66845f4b9ce4c2520
MyLittleChicken <jackbomb51@gmail.com>
2024-06-23 19:19:49 +0900

    Svelte 컴포넌트 구조 기초

---
+++ src/components/MainPage.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Header from './Header.svelte';
+    import Footer from './Footer.svelte';
+    import Side from './Side.svelte';
+</script>
+
+<Header />
+<h1>hello i'm mainPage</h1>
+<Footer />
+<Side />
+
---
+++ src/components/Side.svelte
@@ -0,0 +1,5 @@
+<li>
+    <div>
+        hi im side
+    </div>
+</li>
